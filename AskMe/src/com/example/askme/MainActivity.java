a6f730f2ee9bee1192c58ac6594e79fa69a527b8
y0koz <yoann.ghigoff@gmail.com>
2015-02-20 20:44:50 +0100

    Initial commit

---
+++ AskMe/src/com/example/askme/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.askme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void ask(View view) {
+    	Log.d("ask", "Entrée dans la méthode ask");
+		EditText champQuestion  = (EditText) findViewById(R.id.question);
+		String strQuestion = champQuestion.getText().toString();
+		Log.d("ask", "Question reçu via la méthode ask : " + strQuestion);
+		if(strQuestion.length() < 1) {
+			
+		}
+    }
+}
