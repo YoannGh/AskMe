d507360c80d5e4656459775d7d3ba9a662f86f04
Adrien Pouyet <adrien@pouyet.fr>
2015-03-05 14:40:19 +0100

    lets start bitches !

--- AskMe/src/com/example/askme/MainActivity.java
+++ AskMe/src/com/example/askme/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.askme;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -12,15 +9,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		
 	}
+
+
 	
-	public void ask(View view) {
-    	Log.d("ask", "Entrée dans la méthode ask");
-		EditText champQuestion  = (EditText) findViewById(R.id.question);
-		String strQuestion = champQuestion.getText().toString();
-		Log.d("ask", "Question reçue via la méthode ask : " + strQuestion);
-		if(strQuestion.length() < 1) {
-			
-		}
-    }
+
 }
