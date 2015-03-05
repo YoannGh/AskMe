b189bb7c879e94d8e924cda4dc053d8e2221511c
y0koz <yoann.ghigoff@gmail.com>
2015-03-05 15:03:16 +0100

    test

--- AskMe/src/com/example/askme/MainActivity.java
+++ AskMe/src/com/example/askme/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.askme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.i("commit", "test");
 
 		
 	}
