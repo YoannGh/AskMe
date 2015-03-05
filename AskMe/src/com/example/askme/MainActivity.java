ad83283ba2d7b508b027ddc8e08315e44a65d63a
Briochedu78 <kcierreip.atafa@gmail.com>
2015-03-05 16:55:52 +0100

    tiuzanfdanzd

--- AskMe/src/com/example/askme/MainActivity.java
+++ AskMe/src/com/example/askme/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_result);
+		setContentView(R.layout.activity_main);
 		Log.i("commit", "test");
 	}
 
