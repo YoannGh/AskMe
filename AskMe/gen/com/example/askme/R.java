9fb52c8036a543426b22c99bf2509d30d106a409
Zahirinho <haniche.zahir@gmail.com>
2015-03-05 16:41:38 +0100

    toto

--- AskMe/gen/com/example/askme/R.java
+++ AskMe/gen/com/example/askme/R.java
@@ -800,6 +800,7 @@ containing a value of this type.
         public static final int abc_search_url_text_selected=0x7f070001;
     }
     public static final class dimen {
+        public static final int SizePerso=0x7f080013;
         /**  Default height of an action bar. 
  Default height of an action bar. 
  Default height of an action bar. 
@@ -995,13 +996,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f05003d;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
-        public static final int ask=0x7f05003e;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1023,7 +1024,6 @@ containing a value of this type.
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
-        public static final int question=0x7f05003d;
         public static final int radio=0x7f05002f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
@@ -1042,7 +1042,6 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1149,6 +1148,8 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int ask=0x7f0a0011;
+        public static final int hello_world=0x7f0a0013;
+        public static final int title_activity_main=0x7f0a0012;
         public static final int votre_question=0x7f0a0010;
     }
     public static final class style {
